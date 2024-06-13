9718dc4b8de09ac143a2f97c0e0fc0bff1294450
Gabrielkqw <gabrielkqw@gmail.com>
2024-06-13 18:32:49 -0300

    remove h1 login.tsx

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -4,7 +4,6 @@ import Login from "../../components/login/login";
 const HomePage: React.FC = () => {
   return (
     <div>
-      <h1>Bem-vindo à Página Inicial</h1>
       <Login />
     </div>
   );
