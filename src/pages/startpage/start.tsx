8c6f42206ae0f20d00bd123e5e1b1f9c5c307761
Gabrielkqw <gabrielkqw@gmail.com>
2024-06-15 12:57:02 -0300

    singup-return

--- src/pages/startpage/start.tsx
+++ src/pages/startpage/start.tsx
@@ -1,61 +1,61 @@
-// import React from "react";
-// import { useNavigate } from "react-router-dom";
-// import {
-//   Initial,
-//   Heading,
-//   IconConfig,
-//   Logins,
-//   Content,
-//   PrimaryFooter,
-//   DivFooter,
-//   SecondaryFooter,
-// } from "./StartPage.styles.ts";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Initial,
+  Heading,
+  IconConfig,
+  Logins,
+  Content,
+  PrimaryFooter,
+  DivFooter,
+  SecondaryFooter,
+} from "./StartPage.styles.ts";
 
-// const Home: React.FC = () => {
-//   const navigate = useNavigate();
+const Home: React.FC = () => {
+  const navigate = useNavigate();
 
-//   return (
-//     <Initial>
-//       <Heading>
-//         <IconConfig>
-//           <p>
-//             Cidade
-//             <img src="path_to_your_image" alt="Icon" />
-//             Alt
-//           </p>
-//         </IconConfig>
-//         <Logins>
-//           <p onClick={() => navigate("/login")}>Entrar</p>
-//         </Logins>
-//       </Heading>
-//       <Content>
-//         <p className="text1">Bem-vindo ao GTA Cidade!</p>
-//         <p className="text2">Explore a cidade mais viva do universo GTA.</p>
-//         <p className="text3">Divirta-se e descubra novas aventuras.</p>
-//       </Content>
-//       <PrimaryFooter>
-//         <DivFooter>
-//           <h5>Empresa</h5>
-//           <p>Quem somos</p>
-//         </DivFooter>
-//         <DivFooter>
-//           <h5>Contato</h5>
-//           <p>Email: contato@empresa.com</p>
-//         </DivFooter>
-//         <DivFooter>
-//           <h5>FAQ</h5>
-//           <p>Perguntas frequentes</p>
-//         </DivFooter>
-//         <DivFooter>
-//           <h5>Dicas</h5>
-//           <p>Dicas para iniciantes</p>
-//         </DivFooter>
-//       </PrimaryFooter>
-//       <SecondaryFooter>
-//         <p>© 2024 • GTA cda • Todos os direitos reservados</p>
-//       </SecondaryFooter>
-//     </Initial>
-//   );
-// };
+  return (
+    <Initial>
+      <Heading>
+        <IconConfig>
+          <p>
+            Cidade
+            <img src="path_to_your_image" alt="Icon" />
+            Alt
+          </p>
+        </IconConfig>
+        <Logins>
+          <p onClick={() => navigate("/login")}>Entrar</p>
+        </Logins>
+      </Heading>
+      <Content>
+        <p className="text1">Bem-vindo ao GTA Cidade!</p>
+        <p className="text2">Explore a cidade mais viva do universo GTA.</p>
+        <p className="text3">Divirta-se e descubra novas aventuras.</p>
+      </Content>
+      <PrimaryFooter>
+        <DivFooter>
+          <h5>Empresa</h5>
+          <p>Quem somos</p>
+        </DivFooter>
+        <DivFooter>
+          <h5>Contato</h5>
+          <p>Email: contato@empresa.com</p>
+        </DivFooter>
+        <DivFooter>
+          <h5>FAQ</h5>
+          <p>Perguntas frequentes</p>
+        </DivFooter>
+        <DivFooter>
+          <h5>Dicas</h5>
+          <p>Dicas para iniciantes</p>
+        </DivFooter>
+      </PrimaryFooter>
+      <SecondaryFooter>
+        <p>© 2024 • GTA cda • Todos os direitos reservados</p>
+      </SecondaryFooter>
+    </Initial>
+  );
+};
 
-// export default Home;
+export default Home;
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,21 +1,21 @@
+/** @format */
+
 import Api from "./api";
 import swal from "sweetalert";
-import { UserLogin, RegisterUser } from "../services/interfaces";
+import { UserLogin, RegisterUser } from "./interfaces";
 
 export const loginService = {
   Login: async (values: UserLogin) => {
     try {
       const res = await Api.post("/auth", values);
-      return res.data; // Retorna apenas os dados do response
+      return res;
     } catch (error: any) {
-      console.error("Erro ao realizar login:", error);
       swal({
         title: "Error",
         text: `${error.message}`,
         icon: "error",
         timer: 6000,
       });
-      throw error; // Lança o erro novamente para que o componente que chama possa tratar, se necessário
     }
   },
 };
@@ -24,16 +24,14 @@ export const RegisterService = {
   Register: async (values: RegisterUser) => {
     try {
       const res = await Api.post("/user", values);
-      return res.data; // Retorna apenas os dados do response
+      return res;
     } catch (error: any) {
-      console.error("Erro ao registrar usuário:", error);
       swal({
         title: "Error",
         text: `${error.message}`,
         icon: "error",
         timer: 6000,
       });
-      throw error; // Lança o erro novamente para que o componente que chama possa tratar, se necessário
     }
   },
 };
